bb78056dc30b93a2a3257dfaf79f8c6a46dbe7e9
abhishek <abhiwanttochat@gmail.com>
2017-11-11 12:47:08 +0530

    grouping filterText button and clearFilter button using CssLayout

--- Vaadin_Basics/src/main/java/com/example/Vaadin_Basics/MyUI.java
+++ Vaadin_Basics/src/main/java/com/example/Vaadin_Basics/MyUI.java
@@ -7,14 +7,18 @@ import javax.servlet.annotation.WebServlet;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.data.util.BeanItemContainer;
+import com.vaadin.server.FontAwesome;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.CssLayout;
 import com.vaadin.ui.Grid;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.ValoTheme;
 
 /**
  * This UI is the application entry point. A UI may either represent a browser
@@ -29,26 +33,31 @@ import com.vaadin.ui.VerticalLayout;
 public class MyUI extends UI {
 	private CustomerService service = CustomerService.getInstance();
 	private Grid grid = new Grid();
-	TextField filterText = new TextField("Filter By Name");
+	TextField filterText = new TextField();
 
 	@Override
 	protected void init(VaadinRequest vaadinRequest) {
 
 		VerticalLayout layout = new VerticalLayout();
+		filterText.setInputPrompt("filter by name..");
 		
 		filterText.addTextChangeListener(e -> {
 			grid.setContainerDataSource(new BeanItemContainer<>(Customer.class, 
 					service.findAll(e.getText())));
 		});
 		
-		Button clearFilterTextBtn = new  Button("Clear Filter");
+		Button clearFilterTextBtn = new  Button(FontAwesome.TIMES);
 		clearFilterTextBtn.addClickListener(e -> {
 			filterText.clear();
 			updateList();
 		});
 		
+		CssLayout filtering = new CssLayout();
+		filtering.setStyleName(ValoTheme.LAYOUT_COMPONENT_GROUP);
+		filtering.addComponents(filterText, clearFilterTextBtn);
+		
 		grid.setColumns("firstName", "lastName", "email");
-		layout.addComponents(filterText, clearFilterTextBtn, grid);
+		layout.addComponents(filtering, grid);
 
 		updateList();
 
